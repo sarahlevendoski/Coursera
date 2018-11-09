e1a976d84171456c9eb205442519aa198e96d084
slevendoski <slevendoski@musicnotes.com>
2018-11-09 10:46:21 -0600

    Test for Rat Race Assignment

---
+++ Test_RatRace.py
@@ -0,0 +1,12 @@
+import a2
+import unittest
+
+class TestRatRace(unittest.TestCase):
+    """ Test class for RatRace functions. """
+
+    def test_Rat_Race_is_wall(self):
+    ''' Test the the Rat Race where there is a wall'''
+        
+        expected = 
+        actual = 
+        self.assertEqual(expected, actual)
